d0cdcf10dc302a3a92d6ebe3d2a5246a545af9b4
trieuhd <huutrieuhd@gmail.com>
2017-12-23 18:12:20 +0700

    Second commit

---
+++ webviewclasses/WebPageLongClickListener.java
@@ -0,0 +1,15 @@
+package acr.browser.barebones.webviewclasses;
+
+import acr.browser.barebones.activities.BrowserActivity;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+
+public class WebPageLongClickListener implements OnLongClickListener{
+
+	@Override
+	public boolean onLongClick(View v) {
+	//	return BrowserActivity.onLongClick();
+		return false;
+	}
+
+}
